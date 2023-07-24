e1ad2741e98b5437546cca8f47ed7a825b029e9c
Shawn James <shawn2james@gmail.com>
2023-07-24 13:00:46 +0800

    Completed initial setup of MainFrame
    added title to selection screen:

--- Project_Java/src/controller/MainFrame.java
+++ Project_Java/src/controller/MainFrame.java
@@ -1,77 +1,66 @@
 package controller;
 
-public class MainFrame {
+import java.awt.CardLayout;
 
-	/**
-	 * 
-	 */
+import javax.swing.JFrame;
+
+import gui.SelectionScreen;
+
+public class MainFrame extends JFrame {
 	public Controller controller;
-
-	/**
-	 * 
-	 */
-	public Controller getController() {
-	 	 return controller; 
+	public CardLayout card;
+	
+	public MainFrame() {
+		this.setTitle("Temasek Pizzas");
+		this.setSize(600, 400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.card = new CardLayout();
+		this.setLayout(this.card);
+		this.controller = new Controller();
+		this.showSelectionScreen();
+		this.setVisible(true);
 	}
-
-	/**
-	 * Setter of controller
-	 */
+	
+	public Controller getController() { 
+		return this.controller;
+	}
+	
 	public void setController(Controller controller) { 
 		 this.controller = controller; 
 	}
 
-	/**
-	 * 
-	 */
 	public void showSelectionScreen() { 
-		// TODO Auto-generated method
-	 }
+		SelectionScreen s1 = new SelectionScreen(this);
+		this.add(s1, "Selection");
+		this.card.show(this.getContentPane(), "Selection");
+	}
 
-	public void getController() { 
-		// TODO Auto-generated method
-	 }
-
-	/**
-	 * 
-	 */
 	public void showCustomizeScreen() { 
 		// TODO Auto-generated method
 	 }
 
-	/**
-	 * 
-	 */
 	public void showOrderScreen() { 
 		// TODO Auto-generated method
 	 }
 
-	/**
-	 * 
-	 */
 	public void showStaffMainScreen() { 
 		// TODO Auto-generated method
 	 }
 
-	/**
-	 * 
-	 */
 	public void showStaffLoginScreen() { 
 		// TODO Auto-generated method
 	 }
 
-	/**
-	 * 
-	 */
 	public void showInventoryScreen() { 
 		// TODO Auto-generated method
 	 }
 
-	/**
-	 * 
-	 */
 	public void showMenuScreen() { 
 		// TODO Auto-generated method
 	 } 
+	
+	public static void main(String args[]) {
+		MainFrame gui = new MainFrame();
+	}
 
 }
--- Project_Java/src/gui/SelectionScreen.java
+++ Project_Java/src/gui/SelectionScreen.java
@@ -1,20 +1,22 @@
 package gui;
 
-import javax.swing.JFrame;
-
 import controller.MainFrame;
 
 import javax.swing.JButton;
+import javax.swing.JPanel;
+
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
-public class SelectionScreen extends JFrame {
+public class SelectionScreen extends JPanel {
 	private MainFrame main;
 	
 	public SelectionScreen(MainFrame main) {
 		this.main = main;
-		getContentPane().setLayout(null);
+		setLayout(null);
 		
 		JButton btnCustomerStartOrdering = new JButton("Customer? Start Ordering!");
 		btnCustomerStartOrdering.addActionListener(new ActionListener() {
@@ -23,8 +25,8 @@ public class SelectionScreen extends JFrame {
 			}
 		});
 		btnCustomerStartOrdering.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnCustomerStartOrdering.setBounds(125, 85, 271, 47);
-		getContentPane().add(btnCustomerStartOrdering);
+		btnCustomerStartOrdering.setBounds(141, 137, 271, 47);
+		add(btnCustomerStartOrdering);
 		
 		JButton btnStaff = new JButton("Staff");
 		btnStaff.addActionListener(new ActionListener() {
@@ -33,7 +35,13 @@ public class SelectionScreen extends JFrame {
 			}
 		});
 		btnStaff.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnStaff.setBounds(125, 168, 271, 47);
-		getContentPane().add(btnStaff);
+		btnStaff.setBounds(141, 222, 271, 47);
+		add(btnStaff);
+		
+		JLabel lblTemasekPizzas = new JLabel("Temasek Pizzas");
+		lblTemasekPizzas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTemasekPizzas.setFont(new Font("Berlin Sans FB Demi", Font.BOLD, 29));
+		lblTemasekPizzas.setBounds(114, 36, 312, 47);
+		add(lblTemasekPizzas);
 	} 
 }
